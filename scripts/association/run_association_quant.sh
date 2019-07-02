5fcb7e597e20ad95c6fa4ec6ad7f1ae29d8d7f5d
hemstrow <whemstrom@ucdavis.edu>
2019-07-02 11:24:01 -0700

    added association outputs and scripts

---
+++ scripts/association/run_association_quant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -t 1-12:00:00
+#SBATCH --mem 16G
+#SBATCH -c 16
+
+bamlist=../aus_monarchs/data/gbamlist_minus_bads.txt
+out=association_out_quant
+ybin=../aus_monarchs/data/quant_ybin.txt
+
+minInd=102
+
+~/angsd/angsd -P 16 -bam $bamlist -out $out -minQ 20 -minMapQ 20 -GL 1 -SNP_pval 1e-12 -doMajorMinor 1 -doMaf 2 -minMaf 0.05 -minInd $minInd -doAsso 1 -yBin ${ybin}
+
