6f39b4411623541e9e36fac3bd3a924ab23d4b37
hemstrow <whemstrom@ucdavis.edu>
2020-02-03 18:25:25 -0800

    Added and updated scripts to run primer3

---
+++ scripts/primer_design/prepare_primer3_parms.R
@@ -0,0 +1,14 @@
+args <- commandArgs(T)
+args <- as.character(args)
+parms <- readLines(args[1])
+regions <- readLines(args[2])
+fasta <- readLines(args[3])
+
+parms <- c(paste0("SEQUENCE_ID=", args[4]), paste0("SEQUENCE_TEMPLATE=", fasta), parms)
+out <- vector("list", length(regions))
+
+for(i in 1:length(regions)){
+  out[[i]] <- c(paste0("SEQUENCE_PRIMER_PAIR_OK_REGION_LIST=", regions[i]), parms)
+}
+
+writeLines(unlist(out), paste0("parms_", args[4]), sep = "\n")
--- scripts/primer_design/run_primer3.sh
+++ scripts/primer_design/run_primer3.sh
@@ -3,5 +3,13 @@
 #SBATCH -t 6-12:00:00
 #SBATCH -J primer3
 
-primer3_core --format_output --strict_tags --output=primer3_output.txt < parms
+parms=parms
+fasta=target_region.fasta
+outfile=MonarchMigRegion
+
+Rscript get_primer_intervals.R
+
+Rscript prepare_primer3_parms.R $parms primer_regions.txt $fasta $outfile
+
+primer3_core --format_output --strict_tags --output=${oufile}_primer3_summary.txt < parms_${oufile}
 
